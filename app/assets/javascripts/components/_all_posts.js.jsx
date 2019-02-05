e2c00c566a431a32c9a910daa242f58a16302a2d
Alina Goaga <goaga.alina@gmail.com>
2019-02-05 22:48:26 +0000

    User can delete posts

--- app/assets/javascripts/components/_all_posts.js.jsx
+++ app/assets/javascripts/components/_all_posts.js.jsx
@@ -5,6 +5,7 @@ const AllPosts = (props) => {
         <div key={post.id}>
           <h1>{post.message}</h1>
           <h2>{post.created_at}</h2>
+          <button onClick={ () => props.handleDelete(post.id)}>Delete</button>
         </div>
       )
     })
